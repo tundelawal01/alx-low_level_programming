4a37b6950140e143432775635dff55e4333569d5
tundelawal01 <tundelawal01@github.com>
2023-03-19 11:06:43 -0700

    6-size.c Task

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - This is program that prints the size of a various types of computer
+ * return 0 (Successful)
+ */
+int main(void)
+{
+char q;
+int w;
+long int e;
+long long int r;
+float t;
+
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(q));
+printf("Size of a int: %lu byte(s)\n", (unsigned long)sizeof(w));
+printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(e));
+printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(r));
+printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(t));
+return (0);
+}
