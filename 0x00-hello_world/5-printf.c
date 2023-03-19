8dac66f52aa115524bdf7ac4523476ccc63958d0
tundelawal01 <tundelawal01@github.com>
2023-03-19 10:03:09 -0700

    5-printf.c task

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - this is a program that prints a line with the aid of a printf funtion
+ * Return: 0 (Successful)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n")
+return (0);
+}
