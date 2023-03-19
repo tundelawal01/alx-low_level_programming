4cde2fefd745e6de2943ab3c6a020404b1b49505
tundelawal01 <tundelawal01@github.com>
2023-03-19 09:39:36 -0700

    4-puts.c task

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - This is a program that prints a line with puts function
+ * Return: 0 (succesful)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
