da37e5b2ef00c06c2c6c1aa969aa7b229bc38f9b
tundelawal01 <tundelawal01@github.com>
2023-03-20 09:24:38 -0700

    Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - main ultimate
+ *
+ * Return: ultimate 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		if (i == 9)
+			putchar(i + '0');
+		else
+	putchar(i + '0');
+	putchar(',');
+	putchar(' ');
+	}
+	return (0);
+}
