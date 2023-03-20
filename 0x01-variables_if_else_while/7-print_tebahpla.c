12d51a17827573d324a4b520f64ac966efc9954b
tundelawal01 <tundelawal01@github.com>
2023-03-20 08:39:14 -0700

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - main ultimate
+ *
+ * Return: ultimate (0)
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'z' ; i >= 'a' ; i--)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
