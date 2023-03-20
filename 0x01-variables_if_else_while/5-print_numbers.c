5927e084886465b449fb025da99cf986573e2b2f
tundelawal01 <tundelawal01@github.com>
2023-03-20 07:49:16 -0700

    Numbers Task

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - ultimate main
+ *
+ * Return: ultimate 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+		printf("%d", i);
+	printf("\n");
+	return (0);
+}
