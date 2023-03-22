647629f6a7f64552081b55ce16abf1729e37fe8d
tundelawal01 <tundelawal01@github.com>
2023-03-22 12:24:08 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - ultimate main
+ * _putchar: print the alpabeth putchar
+ * Return: ultimate 0
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
