bd43964f3f042de09b9689c2ce971abbf3bb8fce
tundelawal01 <tundelawal01@github.com>
2023-03-22 22:11:06 -0700

    I sometimes suffer from insomnia.

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabeth - function that prints alphabeth in lower case
+ * Return : ultimate 0;
+ */
+
+
+void print_alphabeth(void)
+		{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		{
+		_putchar(i);
+		}
+		_putchar('\n');
+}
