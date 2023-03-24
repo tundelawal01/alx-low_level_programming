cd5e0c6eb16c376a4c42a61a5fb5c633484273f8
tundelawal01 <tundelawal01@github.com>
2023-03-24 01:18:57 -0700

    I sometimes suffer from insomnia

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,18 @@
 #include "main.h"
 
 /**
- * print_alphabeth - function that prints alphabeth in lower case
+ * print_alphabet - function that prints alphabeth in lower case
+ *
  * Return : ultimate 0;
  */
 
 
-void print_alphabeth(void)
-		{
+void print_alphabet(void)
+
+{
 	char i;
 
-	for (i = 'a'; i <= 'z'; i++)
-		{
+	for (i = 'a' ; i <= 'z' ; i++)
 		_putchar(i);
-		}
-		_putchar('\n');
+	_putchar('\n');
 }
