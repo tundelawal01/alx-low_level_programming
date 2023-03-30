8f70504afd92dd1966e7445b8e93f4933321fcf3
tundelawal01 <tundelawal01@github.com>
2023-03-30 03:38:13 -0700

    Multiple mains Task

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * positive_or_negative - checks for negative or positive integers
+ *
+ * @i: the integers to be checked
+ *
+ * Return: Ultimate 0
+ */
+
+void positive_or_negative(int i)
+{
+	if (i < 0)
+		printf("%d is negative\n", i);
+	else
+		printf("%d is positive\n", i);
+}
